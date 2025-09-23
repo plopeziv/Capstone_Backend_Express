82459d206d1086f8c62aecf9e7d0ec85763e5a91
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-09-22 20:52:34 -0500

    Connect to local postgres db

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const pgp = require("pg-promise");
+const pgp = require("pg-promise")();
+require("dotenv").config();
 
 const app = express();
 const port = 3040;
+const { LOCAL_DB_PASSWORD, DB_NAME } = process.env;
 
-const db = pgp("postgres://postgres:mysecret@localhost:5432/mydb");
+const dbPassword = encodeURIComponent(LOCAL_DB_PASSWORD);
+const db = pgp(`postgres://postgres:${dbPassword}@localhost:5432/${DB_NAME}`);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  try {
+    const data = await db.one("SELECT $1 AS value", 123);
+
+    res.send(`Hello World! Value from DB: ${data.value}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(`Database error: ${err}`);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Capstone Backend listening on port ${port}`);
 });
