9ec0ab8b7a4431e8036e615c7cf33240a5662570
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-09-22 20:04:35 -0500

    Add dotenv

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const pgp = require("pg-promise");
+
 const app = express();
 const port = 3040;
+
+const db = pgp("postgres://postgres:mysecret@localhost:5432/mydb");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
