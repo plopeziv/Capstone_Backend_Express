1f8ef51c035b3075dbae7ab5c936b88d556a9716
Pedro Lopez <plopeziv4@Pedros-MacBook-Pro.local>
2025-10-05 21:08:05 -0500

    Add frontend to cors accepted sites

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 3040;
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://capstone-backend-express-hfplm8xbp.vercel.app",
+    ],
+  })
+);
+
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
